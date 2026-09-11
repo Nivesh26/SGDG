faec530106e1c033b78776bfbb4b685d5ebbd902
Nivesh26 <nivesh.shrestha.94@gmail.com>
2026-09-11 12:08:21 +0545

    style: shorten Hero section text to "Hero"

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
     return (
         <div className="mb-15 mt-15 text-center font-bold text-xl">
-            Hero Section
+            Hero
         </div>
     )
 }
